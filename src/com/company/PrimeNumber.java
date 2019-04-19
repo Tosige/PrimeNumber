0058e1095544c3fe65561918388b9054ccd0278c
Tosin Ige <tosin.ige@gmail.com>
2019-04-19 14:10:23 -0400

    first working code

--- src/com/company/PrimeNumber.java
+++ src/com/company/PrimeNumber.java
@@ -5,38 +5,34 @@ import java.util.Scanner;
 public class PrimeNumber {
 
     public static void main(String[] args) {
-        //Variables
+
+        //variables
         int count;
-        int count2;
         int input;
-        boolean prime = false;
 
         //User inputs random numner
         System.out.print("Enter a random number: ");
         Scanner sc = new Scanner(System.in);
         input = sc.nextInt();
 
-        //while (input >= 1){
-        //Nested For Loop, the first to count from 2 to entered number
-        // and second for modulus calculations
-        for (count = 2; count <= input; count++) {
-
-            for (count2 = 2; count2 <= (input / 2); count2++) {
-
-                //If a number n returns a modulus = 0 when divided by any number
-                //between 2 and half it's value( rounded int), it is not a prime number
-                if (input % count2 == 0) {
-                    prime = false;
-
-                break;
-                }
-            }
-
-            if (prime) {
-
-                System.out.println(count + " is not a prime number");
-            } else
+        for (count =1; count<=input; count++) {
+            if (isPrime(count)) {
                 System.out.println(count + " is a prime number");
+            }
+            else System.out.println(count + " is not a prime number");
+
         }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
